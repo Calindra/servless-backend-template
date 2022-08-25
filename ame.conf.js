8f8964f6d6c2077c063cdf7ff440c756e7bd01a6
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-08-25 18:08:39 -0300

    adicionando ame.conf.js e gitignore

---
+++ ame.conf.js
@@ -0,0 +1,42 @@
+module.exports = {
+  /**
+   * Nome do seu mini app
+   * O nome será utilizado para identificar o seu mini-app dos demais já existentes.
+   */
+  "name": "Meu Mini App",
+  /**
+   * Esta propriedade define o título do mini-app na barra de título do aplicativo
+   */
+  "title": "Meu super mini app",
+  /**
+   * Utilize aqui um identificador único que seja de fácil leitura.
+   * Não utilize espaços em branco.
+   * Sugerimos utilizar um domínio reverso como slug.
+   */
+  "slug": "br.com.minhaempresa.vendas",
+  /**
+   * Aqui você pode incluir a versão do seu mini-app.
+   * Lembre-se que você só poderá enviar 1 versão por vez para publicação.
+   */
+  "version": "0.1.0",
+  /**
+   * Insira os dados da sua empresa,  (Nome da empresa pode conter espaços)
+   * Não esqueça também do seu email de propriedade do mini-app.
+   */
+  "organization": {
+    "name": "Minha Empresa",
+    "owner": {
+      "email": "seuemail@suaempresa.com"
+    },
+  },
+  /**
+   * Aqui nesta linha você determina a versão da biblioteca de componentes que você irá utilizar.
+   * Mais informações em: https://ame-miniapp-components.calindra.com.br/docs/estrutura/
+   */
+  "ame-miniapp-components": "2.6.1",
+  /**
+   * Aqui nesta linha você determina a versão do super-app-client que você irá utilizar.
+   * Mais informações em: https://super-app-client.calindra.com.br/docs/ame-super-app-client/
+   */
+  "ame-super-app-client": "2.9.0",
+}
